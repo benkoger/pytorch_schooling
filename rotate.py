df30c801eeca7b6971526d14426bce534362de15
Ben Koger <benkoger@gmail.com>
2017-08-25 18:53:55 +0200

    turned simulation into a class.  preparing to use matrices for main calculations

--- rotate.py
+++ rotate.py
@@ -2,11 +2,11 @@ import math as m
 import numpy as np
 
 def rotate(vector, degrees):
-	pi_over_180 = m.pi / 180.0
+    pi_over_180 = m.pi / 180.0
 
-	x = vector[0]
-	y = vector[1]
+    x = vector[0]
+    y = vector[1]
 
-	temp_x = ((x * m.cos(degrees * pi_over_180)) - (y * m.sin(degrees * pi_over_180)))
-	temp_y = ((x * m.sin(degrees * pi_over_180)) + (y * m.cos(degrees * pi_over_180)))
-	return (np.array([temp_x, temp_y]))
+    temp_x = ((x * m.cos(degrees * pi_over_180)) - (y * m.sin(degrees * pi_over_180)))
+    temp_y = ((x * m.sin(degrees * pi_over_180)) + (y * m.cos(degrees * pi_over_180)))
+    return (np.array([temp_x, temp_y]))
